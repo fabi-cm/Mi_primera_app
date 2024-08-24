374e00cb64606dbf0981ebce3fb02c13d9a7cbf8
fabi-cm <dahofcmmejia@gmail.com>
2024-08-23 20:35:47 -0400

    Initial commit

---
+++ app/src/main/java/com/example/miprimeraapp/AndroidApplication.kt
@@ -0,0 +1,8 @@
+package com.example.miprimeraapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class AndroidApplication: Application() {
+}
---
+++ app/src/main/java/com/example/miprimeraapp/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.example.miprimeraapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.miprimeraapp.ui.theme.MiPrimeraAppTheme
+import io.sentry.Sentry
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MiPrimeraAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+    Button(onClick = {
+        Sentry.captureException(
+            RuntimeException("This app uses Sentry! xd :3")
+        )
+    }) {
+        Text(text = "Break the world")
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MiPrimeraAppTheme {
+        Greeting("Android")
+    }
+}
